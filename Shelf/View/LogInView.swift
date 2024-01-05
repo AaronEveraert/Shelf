8e47c0507534fc60d4cf80f3b1a661a75a3d3b3a
Aaron <everaertaaron1@gmail.com>
2024-01-05 01:10:33 +0100

    add grid view

---
+++ Shelf/View/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  Shelf
+//
+//  Created by aaron on 29/12/2023.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
